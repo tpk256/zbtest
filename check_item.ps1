b0280f5f5e3dfac2268803d56b21f761a60c9e18
tpk256 <48887274+tpk256@users.noreply.github.com>
2022-08-08 14:49:22 +0500

    update version check item(usb)

--- check_item.ps1
+++ check_item.ps1
@@ -1,4 +1,4 @@
-[boolean] $flag = $false;
+<#[boolean] $flag = $false;
 [string] $obj = "";
 foreach($item in $args)
 {
@@ -16,3 +16,28 @@ foreach($item in Get-PnpDevice)
          break;};
 }
 Write-Host $flag;
+#>
+#  Исправить.
+$firstINT = "Intel(R) RealSense(TM) Depth Camera 415  Depth";
+$secondINT = "Intel(R) RealSense(TM) Depth Camera 415  RGB";
+$List = Get-PnpDevice -PresentOnly | Where-Object{$_.CompatibleID -match "^USB" -and $_.Status -eq "OK" -and ($_.Name -eq $firstINT -or $_.Name -eq $secondINT)} 
+
+foreach($item in $list)
+{
+    $data = @{"DEVPKEY_Device_IsRebootRequired" = $null;
+     "DEVPKEY_Device_HasProblem" = $null;
+     "DEVPKEY_Device_ProblemCode" = $null;
+     };
+
+    $check_items = "DEVPKEY_Device_IsRebootRequired", "DEVPKEY_Device_HasProblem", "DEVPKEY_NAME", "DEVPKEY_Device_ProblemCode";
+
+    foreach($key in $check_items){
+        $data[$key] = (Get-PnpDeviceProperty -InputObject $item -KeyName $key).data;
+        
+        }
+
+        $data;
+}
+
+if (-not $list )
+{write-host $false}
